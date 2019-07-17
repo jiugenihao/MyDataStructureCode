40a483ef7efba83de0f8a9285521df1cb58536f5
hanzhongping <360420509@qq.com>
2019-07-17 10:52:24 +0800

    实现模板单链表

--- TLBBCodeSorting/TLBBCodeSorting/MyLinkedList.cpp
+++ TLBBCodeSorting/TLBBCodeSorting/MyLinkedList.cpp
@@ -2,11 +2,101 @@
 #include "MyLinkedList.h"
 
 
-MyLinkedList::MyLinkedList()
+template <class T>
+MySinglyLinkedList<T>::MySinglyLinkedList()
 {
+	m_Head = nullptr;
+	m_Len = 0;
 }
 
+template <class T>
+MySinglyLinkedList<T>::MySinglyLinkedList(T value[], int n)
+{}
 
-MyLinkedList::~MyLinkedList()
+template <class T>
+MySinglyLinkedList<T>::~MySinglyLinkedList()
 {
+	SAFE_DELETE_ARRAY(m_Head);
+}
+
+template <class T>
+bool MySinglyLinkedList<T>::IsEmpty()
+{
+	return (m_Head == nullptr);
+}
+
+template <class T>
+void MySinglyLinkedList<T>::Clear()
+{
+	
+}
+
+template <class T>
+int MySinglyLinkedList<T>::Len()
+{
+	int len = 0;
+	LinkListNode<T>* p = m_Head;
+	while (nullptr != p)
+	{
+		len++;
+		p = p->m_pNext;
+	}
+	return len;
+}
+
+template <class T>
+LinkListNode<T>* MySinglyLinkedList<T>::GetNode(int pos)
+{
+	if (pos < 0 || IsEmpty())
+		return nullptr;
+
+	int i = 0;
+	LinkListNode<T>* p = m_Head;
+	while (nullptr != p && i < pos)
+	{
+		p = p->m_pNext;
+		i++;
+	}
+	return p;
+}
+
+template <class T>
+bool MySinglyLinkedList<T>::Get(int pos, T& value)
+{
+	LinkListNode<T>* p = GetNode(pos);
+	if (nullptr != p)
+	{
+		value = p->m_Data;
+		return true;
+	}
+	return false;
+}
+
+template <class T>
+bool MySinglyLinkedList<T>::Set(int pos, T value)
+{
+	LinkListNode<T>* p = GetNode(pos);
+	if (nullptr != p)
+	{
+		p->m_Data = value;
+		return true;
+	}
+	return false;
+}
+
+template <class T>
+LinkListNode<T>* MySinglyLinkedList<T>::Insert(int pos, T value)
+{
+	return nullptr;
+}
+
+template <class T>
+void MySinglyLinkedList<T>::Visit()
+{
+	LinkListNode<T>* p = m_Head;
+	while (nullptr != p)
+	{
+		//cout << p->m_Data << endl;
+		p = p->m_pNext;
+	}
 }
--- TLBBCodeSorting/TLBBCodeSorting/MyLinkedList.h
+++ TLBBCodeSorting/TLBBCodeSorting/MyLinkedList.h
@@ -1,13 +1,75 @@
 /////////////////////////////////////////////////////////////////////////////////
 //文件名称：MyLinkedList.h
 //功能描述：利用链表实现的顺序表
+//   MySinglyLinkedList 单链表
 //   查找速度慢，插入删除不需要移动，特别快
 /////////////////////////////////////////////////////////////////////////////////
 #pragma once
-class MyLinkedList
+
+#include "BaseDefine.h"
+
+template <class T>
+class LinkListNode
 {
 public:
-	MyLinkedList();
-	~MyLinkedList();
+	T m_Data;
+	LinkListNode<T>* m_pNext;
+
+public:
+	LinkListNode()
+	{
+		m_pNext = nullptr;
+	}
+	LinkListNode(T data, LinkListNode<T>* next = nullptr)
+	{
+		m_Data = data;
+		m_pNext = next;
+	}
+	~LinkListNode()
+	{
+		m_pNext = nullptr;
+	}
+};
+
+// 单链表
+template <class T>
+class MySinglyLinkedList
+{
+public:
+	MySinglyLinkedList();
+	MySinglyLinkedList(T value[], int n);
+	~MySinglyLinkedList();
+
+public:
+	bool IsEmpty();
+	int	 Len();
+	bool Get(int pos, T& value);
+	bool Set(int pos, T value);
+	LinkListNode<T>* GetNode(int pos);
+	LinkListNode<T>* Insert(int pos, T value);
+	bool Remove(int pos, T& old);
+	void Clear();
+	void Concat(MySinglyLinkedList<T>& list);
+	void Visit();
+
+	friend ostream& operator << (ostream& out, MySinglyLinkedList<T>& list)
+	{
+		out << "output all elements:" << endl;
+		LinkListNode<T>* p = list.m_Head;
+		while (nullptr != p)
+		{
+			out << p->m_Data;
+			p = p->m_pNext;
+			if (nullptr != p)
+				out << "-";
+		}
+		out << endl;
+		return out;
+	}
+
+private:
+	LinkListNode<T>* m_Head;
+	int m_Len;
+
 };
 
